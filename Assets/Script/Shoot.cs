05e4577511ba0d622982a1d87bf67f2620bd3bad
ssaemo <jeontha@gmail.com>
2017-11-04 14:39:21 +0900

    Cannon, Bullet Obj Created

---
+++ Assets/Script/Shoot.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shoot : MonoBehaviour
+{
+    public const int FORCE = 3000;
+    public GameObject bulletPrefab;
+
+	void Start ()
+    {
+        Debug.Assert(bulletPrefab);
+	}
+	
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("Shoot");
+            GameObject bulletInstance = Instantiate(bulletPrefab, transform.position, transform.rotation);
+            var rigid = bulletInstance.GetComponent<Rigidbody>();
+            Debug.Assert(rigid);
+            rigid.AddForce(transform.forward * FORCE);
+        }
+	}
+}
