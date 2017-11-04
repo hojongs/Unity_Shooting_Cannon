5ef071389b92e1133d7ca9f609ff3b71a5c1f685
ssaemo <jeontha@gmail.com>
2017-11-04 17:24:45 +0900

    Unity C# lambda callback example, (cannot use with SendMessage())

---
+++ Assets/Script/Interactee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Interactee : MonoBehaviour
+{
+    string myName;
+
+    void Start ()
+    {
+        myName = "interactee";
+	}
+	
+	void Update ()
+    {
+		
+	}
+
+    public void interact(Action lambda)
+    {
+        lambda();
+        Debug.Log(myName);
+    }
+}
