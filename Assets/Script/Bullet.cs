02987bc71612aa0c4faeb982d7d2147d9a182aaa
ssaemo <jeontha@gmail.com>
2017-11-07 23:30:42 +0900

    complete Bullet Shooting (on every client)

---
+++ Assets/Script/Bullet.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    [HideInInspector]
+    public GameObject shooterObj;
+
+	void Start () { }
+	
+	void Update () { }
+}
---
+++ Assets/Script/Shooter.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Shooter : NetworkBehaviour
+{
+    public const int POWER = 3000;
+    public GameObject bulletPrefab;
+
+    void Start()
+    {
+        Debug.Assert(bulletPrefab);
+
+        InvokeRepeating("pollShoot", 0, 0.1f);
+    }
+
+    void Update() { }
+
+    void pollShoot()
+    {
+        if (!isLocalPlayer)
+            return;
+
+        if (Input.GetAxis("Fire1") != 0)
+            CmdShoot();
+    }
+
+    [Command] // run by only server
+    void CmdShoot()
+    {
+        Debug.Log("CmdShoot()");
+
+        var pos = transform.position + transform.forward;
+
+        GameObject bulletObj = Instantiate(bulletPrefab, pos, transform.rotation);
+        Debug.Assert(bulletObj);
+
+        NetworkServer.Spawn(bulletObj); // spawn by all client
+        RpcAddForceOnAll(bulletObj);
+    }
+
+    [ClientRpc]
+    void RpcAddForceOnAll(GameObject bulletObj)
+    {
+        Bullet compoBullet = bulletObj.GetComponent<Bullet>();
+        Rigidbody compoRigid = bulletObj.GetComponent<Rigidbody>();
+        Debug.Assert(compoBullet && compoRigid);
+
+        compoBullet.shooterObj = gameObject;
+        compoRigid.AddForce(transform.forward * POWER);
+    }
+}
